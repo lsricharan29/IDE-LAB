c226e5d4e953ac6cd09ea52af80500cf48a6388a
lsricharan29 <70264256+lsricharan29@users.noreply.github.com>
2020-09-30 15:33:03 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,66 @@
+
+		import java.awt.*;
+		import java.awt.event.*;
+
+		import javax.swing.*;
+
+		public class add extends JFrame implements ActionListener
+		{
+		 JLabel l1,l2,l3;
+		 JButton b1;
+		 JTextField t1,t2,t3;
+		 
+		 add()
+		 {
+		  l1=new JLabel("INPUT 1");
+		  l2=new JLabel("INPUT 2");
+		  
+		  b1=new JButton("sum");
+		  l3=new JLabel("OUTPUT");
+		  
+		  t1=new JTextField(10);
+		  t2=new JTextField(10);
+		  t3=new JTextField(10);
+		  
+		  add(l1);
+		  add(t1);
+		  add(l2);
+		  add(t2);
+		  add(l3);
+		  add(t3);
+		  add(b1);
+		  
+		  b1.addActionListener(this);
+		  
+		  setSize(200,200);
+		  setLayout(new FlowLayout());
+		  setTitle("Addition");
+		 }
+
+		 public void actionPerformed(ActionEvent ae)
+		 {
+		  float a,b,c;
+		  if(ae.getSource()==b1)
+		  {
+		   a=Float.parseFloat(t1.getText());
+		   b=Float.parseFloat(t2.getText());
+		   c=a+b;
+		   t3.setText(String.valueOf(c));
+		   
+		  }
+		  
+		  
+		 }
+
+		 public static void main(String args[])
+		 {
+		  add a=new add();
+		  a.setVisible(true);
+		  a.setLocation(200,200);
+		  
+		 }
+
+		}
+
+
+
